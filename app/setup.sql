1146adfc192ff9fbdb8119ae893ef48d0471a836
verwann <erwann.varlet@ynov.com>
2024-10-11 17:20:22 +0200

    upgrade sql table

--- app/setup.sql
+++ app/setup.sql
@@ -9,37 +9,43 @@ cv_db;
 -- Create table for users
 CREATE TABLE user
 (
-    id         INT PRIMARY KEY AUTO_INCREMENT,
-    email      VARCHAR(255) NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    last_name  VARCHAR(100) NOT NULL,
-    password   VARCHAR(255) NOT NULL,
-    admin      BOOLEAN      NOT NULL
+    id         UUID PRIMARY KEY,
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    first_name VARCHAR(100)        NOT NULL,
+    last_name  VARCHAR(100)        NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    admin      BOOLEAN             NOT NULL DEFAULT false
 );
 
-INSERT INTO user (email, first_name, last_name, password, admin)
-VALUES ('admin@exemple.com', 'admin', '_', 'admin', true)
+INSERT INTO user (id, email, first_name, last_name, password, admin)
+VALUES ('f15fab2b-e769-4bdf-8f17-7f58ee4cfa5d', 'admin@exemple.com', 'admin', '_', 'admin', true);
 
 -- Create table for users
 CREATE TABLE cv
 (
-    id           INT PRIMARY KEY AUTO_INCREMENT,
-    creator_id   INT          NOT NULL,
+    id           UUID PRIMARY KEY,
+    creator_id   UUID UNIQUE  NOT NULL,
     title        VARCHAR(255) NOT NULL,
     description  TEXT,
-    skills       JSON COMMENT 'Structure: type string[]',
-    certificates JSON COMMENT 'Structure: level (string), school (string), date (year)',
-    experiences  JSON COMMENT 'Structure: post (string), company (string), start_date (year), end_date (year)'
+    skills       JSON COMMENT 'Type: string[]',
+    certificates json COMMENT 'Structure: level (string), school (string), date (year)',
+    experiences  json COMMENT 'Structure: post (string), company (string), start_date (year), end_date (year)'
 );
 
 -- Create table for project for portfolio
 CREATE TABLE project
 (
-    id          INT PRIMARY KEY AUTO_INCREMENT,
-    creator_id  INT          NOT NULL,
+    id          UUID PRIMARY KEY,
+    creator_id  UUID         NOT NULL,
     title       VARCHAR(255) NOT NULL,
-    description TEXT,
+    description TEXT         NOT NULL,
     theme       VARCHAR(100),
     link        VARCHAR(255),
-    images      JSON COMMENT 'Structure: type string[]'
+    images      JSON COMMENT 'Type: string[]'
 );
+
+-- Create foreign keys
+ALTER TABLE cv
+    ADD FOREIGN KEY (creator_id) REFERENCES user (id);
+ALTER TABLE project
+    ADD FOREIGN KEY (creator_id) REFERENCES user (id);
