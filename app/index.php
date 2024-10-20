7c7ab7cb1e2ead905e795ab54ffa5539f6c4f279
verwann <erwann.varlet@ynov.com>
2024-10-20 15:13:52 +0200

    update project cards on index.php

--- app/index.php
+++ app/index.php
@@ -233,7 +233,7 @@ $userInfo = getUserInfo($_SESSION['user_id'] ?? 0);
         <?php
         $project_data = [];
         if (isset($_SESSION['user_id'])) {
-            $stmt = $pdo->prepare('SELECT title, description, theme, link, images FROM project WHERE creator_id = :id');
+            $stmt = $pdo->prepare('SELECT id, title, description, theme, link, images FROM project WHERE creator_id = :id LIMIT 3');
             $stmt->execute(array('id' => $_SESSION['user_id']));
             $project_data = $stmt->fetchAll();
         }
@@ -243,39 +243,43 @@ $userInfo = getUserInfo($_SESSION['user_id'] ?? 0);
             <h2 class="text-center mb-4">Mes Projets</h2>
             <div class="row" style="justify-content: center;">
                 <?php
-                function displayProjectCard($title, $theme, $link, $description, array $images, int $index)
+                function displayProjectCard($id, $title, $description, $theme, $link, array $images, int $index): void
                 {
                     echo '<div class="col-md-4 mb-4">
-                    <div class="card">';
+                                <div class="card">';
                     if ($images) {
-                        echo '<div id="carouselProject' . $index . '" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-indicators">';
-                        for ($img_i = 0; $img_i < count($images); $img_i++)
-                            echo  '<button type="button" data-bs-target="#carouselProject' . $index . '" data-bs-slide-to="' . $img_i . '"' . ($img_i == 0 ? 'class="active" aria-current="true"' : '') . '></button>';
-                        echo   '</div>
-                            <div class="carousel-inner">';
-                        foreach ($images as $i => $img)
-                            echo '<div class="carousel-item' . ($i == 0 ? ' active' : '') . '">
-                                    <img src="img/' . $img . '" class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" alt="projet_image">
+                        if (count($images) > 1) {
+                            echo '  <div id="carouselProject-' . $index . '" class="carousel slide" data-bs-ride="carousel">
+                                    <div class="carousel-indicators">';
+                            foreach ($images as $image_i => $image)
+                                echo '           <button type="button" data-bs-target="#carouselProject-' . $index . '" 
+                                            data-bs-slide-to="' . $image_i . '"' . ($image_i == 0 ? ' class="active" aria-current="true"' : '') . '>
+                                        </button>';
+                            echo '          </div>
+                                    <div class="carousel-inner">';
+                            foreach ($images as $image_i => $image)
+                                echo '        <div class="carousel-item ' . ($image_i == 0 ? ' active' : '') . '">
+                                            <img src="img/projects/' . $id . '/' . $image . '" class="project-image bd-placeholder-img bd-placeholder-img-lg d-block w-100" alt="project_image-' . $image_i . '">
+                                        </div>';
+                            echo '          </div>
+                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselProject-' . $index . '" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselProject-' . $index . '" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>
                                 </div>';
-                        echo '</div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselProject' . $index . '" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselProject' . $index . '" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
-                        </div>';
-                    }
-                    echo '<div class="card-body">
-                            <div class="d-flex flex-row align-items-center">
-                                <h5 class="card-title p-2">' . $title . '</h5>' .
-                        ($theme ? '<span class="badge rounded-pill text-bg-primary ms-2">' . $theme . '</span>' : '') .
+                        } else echo '<img src="img/projects/' . $id . '/' . $images[0] . '" class="card-img-top project-image" alt="no image project">';
+                    } else echo '<img src="img/projects/no_img.png" class="card-img-top project-image" alt="no image project">';
+                    echo '<div class="card-body d-flex flex-column">
+                            <div class="d-flex flex-wrap align-items-center mb-2">
+                                <h5 class="card-title p-2 mb-0">' . $title . '</h5>
+                                <span class="badge rounded-pill text-bg-primary">' . $theme . '</span>'.
                         ($link ? '<a href="' . $link . '" target="_blank" class="btn btn-sm btn-primary btn-custom ms-auto">Voir le projet</a>' : '') .
                             '</div>
-                            <p class="card-text mb-1">' . $description . '</p>
+                            <p class="card-text mb-1">' . nl2br($description) . '</p>
                         </div>
                     </div>
                 </div>';
@@ -283,20 +287,21 @@ $userInfo = getUserInfo($_SESSION['user_id'] ?? 0);
 
                 if ($project_data) {
                     foreach ($project_data as $project_i => $project)
-                        if ($project_i < 3)
-                            displayProjectCard(
-                                    $project['title'],
-                                    $project['theme'],
-                                    $project['link'],
-                                    $project['description'],
-                                    json_decode($project['images'], true),
-                                    $project_i
-                            );
+                        displayProjectCard(
+                            $project['id'],
+                            $project['title'],
+                            $project['description'],
+                            $project['theme'],
+                            $project['link'],
+                            json_decode($project['images'], true),
+                            $project_i
+                        );
                 } else displayProjectCard(
+                    -1,
                     'Pas de projet enregistrée',
-                    '',
-                    '',
                     isset($_SESSION['user_id']) ? 'Gerez et  ajoutez vos projets personnels et professionnels' : 'Connectez vous pour afficher vos projets personnels et professionnels',
+                    '',
+                    '',
                     [],
                     0
                 );
